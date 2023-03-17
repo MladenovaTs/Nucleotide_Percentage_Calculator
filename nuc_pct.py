364269fb1cbe5545198a1f8034def735e4c1d8de
Mladenova <cvetelina.ml@gmail.com>
2023-03-17 16:33:46 +0000

    Corrected Python script for DNA or RNA sequence as input and computes the percentage of each nucleotide

--- nuc_pct.py
+++ nuc_pct.py
@@ -11,14 +11,14 @@ def nucleotide_percentages(seq):
     """
     seq = seq.upper()  # convert the sequence to uppercase to make it case-insensitive
     nuc_counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'U': 0}  # initialize a dictionary to count the nucleotides
-    
+
     for nuc in seq:
         if nuc in nuc_counts:
             nuc_counts[nuc] += 1  # increment the count for the corresponding nucleotide
-    
+
     seq_len = len(seq)
     nuc_pct = {nuc: round((count / seq_len) * 100, 2) for nuc, count in nuc_counts.items()}  # compute the percentages
-    
+
 
     return nuc_pct
     print (nuc_pct)
